7271427e88a0fba47a5821e8638e69f7075555a8
kasoban <git@kasoban.de>
2017-01-20 20:06:57 +0100

    initial project shell

---
+++ html/src/bonsai/dev/ggj2017/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package bonsai.dev.ggj2017.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import bonsai.dev.ggj2017.WavesGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new WavesGame();
+        }
+}
